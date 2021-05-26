579739b1869589b1d8b6389eeaa8be514ee17de7
CryptoThibault <thibault.chalaoux@gmail.com>
2021-05-26 12:47:05 +0200

    package

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,6 @@
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-solhint');
+
 module.exports = {
-  solidity: "0.8.4",
+  solidity: '0.8.4',
 };
